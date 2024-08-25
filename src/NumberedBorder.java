1ecaf30c634934492bc0165d78263c2cdf1c9353
Diego Borba <diegoborba25.contato@gmail.com>
2024-08-25 15:50:00 -0300

    refactor: NumberedBorder improved

--- src/NumberedBorder.java
+++ src/NumberedBorder.java
@@ -1,36 +1,16 @@
-
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Insets;
-import javax.swing.JTextArea;
+import javax.swing.*;
 import javax.swing.border.AbstractBorder;
+import java.awt.*;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author steph
- */
 public class NumberedBorder extends AbstractBorder {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -5089118025935944759L;
 
-    private static int lineHeight;
-    private final int characterHeight = 8;
-    private final int characterWidth = 7;
-    private final Color myColor;
+    private static final int CHARACTER_HEIGHT = 8;
+    private static final int CHARACTER_WIDTH = 7;
+    private static final Color COLOR = new Color(164, 164, 164);
 
-    NumberedBorder() {
-        myColor = new Color(164, 164, 164);
-    }
+    private static int lineHeight;
 
     @Override
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
@@ -40,31 +20,21 @@ public class NumberedBorder extends AbstractBorder {
         lineHeight = metrics.getHeight();
 
         Color oldColor = g.getColor();
-        g.setColor(myColor);
+        g.setColor(COLOR);
 
-        double r = (double) height / (double) lineHeight;
-        int rows = (int) (r + 0.5);
-        String str = String.valueOf(rows);
         int lineLeft = calculateLeft(height) + 10;
-
-        int px = 0;
-        int py = 0;
-        int lenght = 0;
 
         int visibleLines = textArea.getHeight() / lineHeight;
         for (int i = 0; i < visibleLines; i++) {
+            String str = String.valueOf(i + 1);
+            int lenght = str.length();
 
-            str = String.valueOf(i + 1);
-            lenght = str.length();
-
-            py = lineHeight * i + 14;
-            px = lineLeft - (characterWidth * lenght) - 2;
+            int py = lineHeight * i + 14;
+            int px = lineLeft - (CHARACTER_WIDTH * lenght) - 2;
 
             g.drawString(str, px, py);
         }
-
         g.drawLine(lineLeft, 0, lineLeft, height);
-
         g.setColor(oldColor);
     }
 
@@ -79,8 +49,6 @@ public class NumberedBorder extends AbstractBorder {
         int rows = (int) (r + 0.5);
         String str = String.valueOf(rows);
         int lenght = str.length();
-        return characterHeight * lenght;
-
+        return CHARACTER_HEIGHT * lenght;
     }
-
 }
