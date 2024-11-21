932484ee6a1ea5574aea3d8407dd77ecde76f16b
Stephanie Possamai <stephaniepossamaiamanda@gmail.com>
2024-11-21 19:04:53 -0300

    feat:novosConstants

--- src/AnalizadorLexico/Constants.java
+++ src/AnalizadorLexico/Constants.java
@@ -1,9 +1,7 @@
-package AnalizadorLexico;
-import AnalizadorLexico.ParserConstants;
-
-public interface Constants extends ScannerConstants, ParserConstants {
-    int EPSILON = 0;
-    int DOLLAR = 1;
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
 
     int t_palavra_reservada = 2;
     int t_main = 3;
@@ -39,4 +37,5 @@ public interface Constants extends ScannerConstants, ParserConstants {
     int t_TOKEN_33 = 33; //"="
     int t_TOKEN_34 = 34; //"("
     int t_TOKEN_35 = 35; //")"
+
 }
